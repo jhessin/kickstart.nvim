fb3736a00f504b89462d174d7bbbc21fb1888799
Jim Hessin <jhessin@gmail.com>
2023-07-10 15:47:25 -0600

    Mon Jul 10 15:47:28 2023

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,1 +1,13 @@
 vim.o.textwidth = 80
+
+-- local function close()
+--   vim.cmd("echo 'It Works!'")
+-- --   This isn't working for some reason
+--   vim.cmd("normal ?##<CR>A - submitted - closed<ESC>")
+-- end
+
+vim.api.nvim_create_user_command('Job', 'normal ?##<CR>A - submitted - closed<ESC>', {})
+vim.api.nvim_create_user_command('Rename', 'normal IF_<ESC>pa_<ESC>A - *<ESC>', {})
+-- vim.api.nvim_create_user_command('JD', function()
+--   vim.cmd("normal ?##<CR>A - submitted - closed<ESC>")
+-- end, {})
