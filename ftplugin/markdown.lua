09649cf7a15306f2db41d7d48517ea2b388ca256
Jim Hessin <jhessin@gmail.com>
2023-07-11 16:46:36 -0600

    Tue Jul 11 16:46:41 2023

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,14 +1,18 @@
 vim.o.textwidth = 80
 
--- local function close()
---   vim.cmd("echo 'It Works!'")
--- --   This isn't working for some reason
---   vim.cmd("normal ?##<CR>A - submitted - closed<ESC>")
--- end
+local function setup()
+  vim.cmd("echo 'It Works!'")
+  vim.cmd("cd ~/Documents/work-files")
+  vim.cmd("split comments.txt")
+  vim.cmd("vsplit port-pkg-notes.md")
+  --   This isn't working for some reason
+  -- vim.cmd("normal ?##<CR>A - submitted - closed<ESC>")
+end
 
 vim.api.nvim_create_user_command('Job', 'normal ?##<CR>A - submitted - closed<ESC>', {})
 vim.api.nvim_create_user_command('Rename', 'normal IF_<ESC>pa_<ESC>A - *<ESC>', {})
 vim.api.nvim_create_user_command('InitJob', 'normal I## <ESC>^eea<CR><ESC>VS`jjo<ESC>', {})
+vim.api.nvim_create_user_command('Setup', function() setup() end, {})
 -- vim.api.nvim_create_user_command('JD', function()
 --   vim.cmd("normal ?##<CR>A - submitted - closed<ESC>")
 -- end, {})
