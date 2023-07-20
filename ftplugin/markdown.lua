d60460729730fe9e13edd7d9f6731fac32cabdea
Jim Hessin <jhessin@advanced1.net>
2023-07-20 09:38:04 -0500

    Fixed work functions

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -5,23 +5,28 @@ local function setup()
   vim.cmd("cd ~/Documents/work-files")
   vim.cmd("split comments.txt")
   vim.cmd("vsplit port-pkg-notes.md")
-  --   This isn't working for some reason
-  -- vim.cmd("normal ?##<CR>A - submitted - closed<ESC>")
 end
 
 local function setupMaps()
   vim.cmd("echo 'It Works!'")
   vim.cmd("cd ~/Documents/work-files")
-  vim.cmd("vsplit INDY-notes.md")
-  --   This isn't working for some reason
-  -- vim.cmd("normal ?##<CR>A - submitted - closed<ESC>")
+  vim.cmd("split Maps/show_footage.bc")
+end
+
+local function measure()
+  vim.cmd("write")
+  vim.cmd("!bc Maps/show_footage.bc")
+end
+
+local function initJob(count)
+  for i = 1, count do
+    vim.cmd.normal(vim.api.nvim_replace_termcodes("I## <ESC>^eea<CR><ESC>VS`jjo<ESC>j", true, true, true))
+  end
 end
 
 vim.api.nvim_create_user_command('Job', 'normal ?##<CR>A - submitted - closed<ESC>', {})
 vim.api.nvim_create_user_command('Rename', 'normal IF_<ESC>pa_<ESC>A - *<ESC>', {})
-vim.api.nvim_create_user_command('InitJob', 'normal I## <ESC>^eea<CR><ESC>VS`jjo<ESC>', {})
+vim.api.nvim_create_user_command('InitJob', function(opts) initJob(opts.fargs[1]) end, { nargs = 1})
 vim.api.nvim_create_user_command('Setup', function() setup() end, {})
 vim.api.nvim_create_user_command('SetupMaps', function() setupMaps() end, {})
--- vim.api.nvim_create_user_command('JD', function()
---   vim.cmd("normal ?##<CR>A - submitted - closed<ESC>")
--- end, {})
+vim.api.nvim_create_user_command('Measure', function() measure() end, {})
