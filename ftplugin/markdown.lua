c2e2f76e340567e05bb4fa5c8005f37594607600
Jim Hessin <jhessin@advanced1.net>
2023-09-26 06:36:14 -0500

    Tue Sep 26 06:36:25 2023

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,43 +1,5 @@
 vim.o.textwidth = 80
 
-<<<<<<< HEAD
--- local function setup()
---   vim.cmd("cd ~/Documents/work-files")
---   vim.cmd("split Preterms/comments.txt")
---   vim.cmd("vsplit Preterms/pretermNotes.md")
---   vim.cmd("split Preterms/DWDM_Channels.txt")
--- end
---
--- local function setupAutoCad()
---   vim.cmd("cd ~/Documents/work-files")
---   vim.cmd("split AutoCad/notes.txt")
--- end
---
--- local function measure()
---   vim.cmd("write")
---   vim.cmd("!bc C:/Users/jhessin/Documents/MapsToWork/show_footage.bc")
--- end
---
--- local function initJob(count)
---   for _ = 1, count do
---     vim.cmd.normal(vim.api.nvim_replace_termcodes("I## <ESC>^eea<CR><ESC>VS`jjo<ESC>j", true, true, true))
---   end
--- end
---
--- local function initMap(count)
---   for _ = 1, count do
---     vim.cmd.normal(vim.api.nvim_replace_termcodes("I### <ESC>^eea<CR><ESC>VS`jjo<ESC>j", true, true, true))
---   end
--- end
---
--- vim.api.nvim_create_user_command('Job', 'normal ?##<CR>A - submitted - closed<ESC>', {})
--- vim.api.nvim_create_user_command('Rename', 'normal IF_<ESC>pa_<ESC>A - *<ESC>', {})
--- vim.api.nvim_create_user_command('InitJob', function(opts) initJob(opts.fargs[1]) end, { nargs = 1 })
--- vim.api.nvim_create_user_command('InitMap', function(opts) initMap(opts.fargs[1]) end, { nargs = 1 })
--- vim.api.nvim_create_user_command('Setup', function() setup() end, {})
--- vim.api.nvim_create_user_command('setupAutoCad', function() setupAutoCad() end, {})
--- vim.api.nvim_create_user_command('Measure', function() measure() end, {})
-=======
 local function setup()
   vim.cmd("cd ~/Documents/work-files")
   vim.cmd("split Preterms/comments.txt")
@@ -74,4 +36,3 @@ vim.api.nvim_create_user_command('InitMap', function(opts) initMap(opts.fargs[1]
 vim.api.nvim_create_user_command('Setup', function() setup() end, {})
 vim.api.nvim_create_user_command('SetupAutoCad', function() setupAutoCad() end, {})
 vim.api.nvim_create_user_command('Measure', function() measure() end, {})
->>>>>>> 50751c3ba939814a6137e95b14b9eded724b536e
