872957efd7fbd4338fb63b7db650893b25eb7be1
Jim Hessin <jhessin@gmail.com>
2024-11-23 07:26:37 -0700

    Sat Nov 23 07:26:38 2024

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,9 @@
 return {
   -- You can add your own plugins here or in other files in this directory!
 
+  -- async
+  "nvim-neotest/nvim-nio",
+
   -- automatically create directories
   'dockyard/vim-easydir',
 
